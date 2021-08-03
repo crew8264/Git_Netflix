bfd59b40fe1c187efb07efa3250f638e3c717abe
crew8264 <crew8264@gmail.com>
2021-08-04 03:05:10 +0900

    Update lang.js
    second.

--- NETFLIX_CLONE_INIT/js/lang.js
+++ NETFLIX_CLONE_INIT/js/lang.js
@@ -9,7 +9,7 @@ $('.lang-selector').on('change', function () {
         $.ajax({
             type: 'GET',
             dataType: 'json',
-            url: '/assets/lang/en.json',
+            url: 'Git_Netflix/NETFLIX_CLONE_INIT/assets/lang/en.json',
             success: function (data) {
                 langData = data;
                 changeLanguage(langData);
@@ -58,7 +58,7 @@ $(function () {
         $.ajax({
             type: 'GET',
             dataType: 'json',
-            url: '/assets/lang/en.json',
+            url: 'Git_Netflix/NETFLIX_CLONE_INIT/assets/lang/en.json',
             success: function (data) {
                 langData = data;
                 changeLanguage(langData);
